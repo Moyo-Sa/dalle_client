11beda39184ad96adc0dcf58f10214d74f42ed00
Moyo-Sa <128077572+Moyo-Sa@users.noreply.github.com>
2024-09-11 13:41:40 -0400

    Update vite.config.js with appropriate links

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: "https://dalle-server-yq30.onrender.com",
+        changeOrigin: true,
+      },
+    },
+  }
 })
